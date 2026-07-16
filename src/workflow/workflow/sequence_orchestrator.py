f737b9cc82585f4436acc050d1f0ca8b00ca7c5d
S-Astralcoder <suraj.astralcode.ai@gmail.com>
2026-07-16 21:37:14 +0530

    feat: Enhance SequenceOperations with validation methods for create, copy, and move actions
    - Introduced methods to validate and update state for create, copy, and move operations within the SequenceOperations class.
    - Added workspace scope validation to prevent operations outside the configured workspace.
    - Improved error handling with detailed messages for invalid paths and operations.
    - Refactored path handling to utilize pathlib for better path management.

--- src/workflow/workflow/sequence_orchestrator.py
+++ src/workflow/workflow/sequence_orchestrator.py
@@ -1,26 +1,94 @@
 
-import pathlib
+from pathlib import Path
 from typing import Any
 
-from workflow.exceptions import InvalidWorkFlowScript
+from workflow.exceptions import InvalidWorkFlowScript, OutOfScope, WorkspaceProtection
+from workflow.safety import FileSafety
 
 
 class SequenceOperations:
     def __init__(self, workflow_data : dict[Any, Any]) -> None:
         self.workflow_data = workflow_data
 
-        self.workspace = pathlib.Path(self.workflow_data["meta-data"].get("workspace")).resolve()
+        self.force = self.workflow_data["meta-data"].get("force")
+
+        self.workspace = Path(self.workflow_data["meta-data"].get("workspace")).resolve()
 
         # internal space
-        self.would_exits : set[pathlib.Path] = set()
-        self.would_removed : set[pathlib.Path] = set()
+        self.would_exits : set[Path] = set() # Update to tree structure in next version. when implementing full scale dynamic execution
+        self.would_removed : set[Path] = set()
 
 
     def initial_setup(self):
         self.would_exits.add(self.workspace)
 
     def validate_create_and_update_state(self, action_data : dict[Any, Any]):
+        path = Path(action_data.get("path")).resolve()  # pyright: ignore[reportArgumentType]
+        recursive = action_data.get("recursive")
+        try:
+            self.validate_workspace_scope(self.workspace, path)  
+        except SystemExit as e:
+            raise InvalidWorkFlowScript(e) # unnecessary double raise but nah lets to it for consistency
+        if not recursive:
+            if not FileSafety.does_exists(path.parent):
+                if not path.parent.resolve() in self.would_exits:
+                    raise InvalidWorkFlowScript(f"The parent for the operation create in action {action_data.get("id")} doesn't exist, use --recursive")
+            self.would_exits.add(path.resolve())
+        else:
+            recursive_path = path
+            while not recursive_path.resolve() in self.would_exits:
+                self.would_exits.add(recursive_path.resolve())
+                recursive_path = recursive_path.parent
+
+    def validate_copy_and_update_state(self, action_data : dict[Any, Any]):
+        source_path = Path(action_data.get("source_path")).resolve()  # pyright: ignore[reportArgumentType]
+        destination_path = Path(action_data.get("destination_path")).resolve()  # pyright: ignore[reportArgumentType]
+        try:
+            self.validate_workspace_scope(self.workspace, source_path)  
+            self.validate_workspace_scope(self.workspace, destination_path)  
+        except SystemExit as e:
+            raise InvalidWorkFlowScript(e) 
+
+        from_state_space : bool = False
+
+        # source validation
+        if not FileSafety.does_exists(path=source_path):
+            if not source_path.resolve() in self.would_exits:
+                raise InvalidWorkFlowScript(f"The given source path to be copied doesn't exist in action {action_data.get("id")}")
+            from_state_space = True
+        if not self.force:
+            if FileSafety.does_exists(path=destination_path / source_path.name):
+                raise InvalidWorkFlowScript(f"The given source already exists in destination in action {action_data.get("id")}")
+
+        if not FileSafety.does_exists(path=destination_path):
+            if not destination_path.resolve() in self.would_exits:
+                raise InvalidWorkFlowScript(f"The given destination doesn't exist in action {action_data.get("id")}")
+
+        if FileSafety.check_if_file(path=destination_path):
+            raise InvalidWorkFlowScript(f"The given destination should be a folder in action {action_data.get("id")}")
+
+        if FileSafety.is_relative_to(path1=source_path, path2=destination_path):
+            raise InvalidWorkFlowScript(f"Coping itself to it's own child is not allowed in action {action_data.get("id")}")
+        
+        if not FileSafety.check_if_file(path=source_path):
+            if from_state_space:
+                for virtual_path in self.would_exits:
+                    if FileSafety.is_relative_to(path1=source_path, path2=virtual_path):
+                        new_virtual_path = destination_path / Path(*virtual_path.parts[virtual_path.parts.index(source_path.name):])
+                        self.would_exits.add(new_virtual_path.resolve())
+            else:
+                new_virtual_path = destination_path / source_path.name
+                self.would_exits.add(new_virtual_path.resolve())
+                for child in source_path.rglob("*"):
+                    new_virtual_path = destination_path / Path(*child.parts[child.parts.index(source_path.name):])
+                    self.would_exits.add(new_virtual_path.resolve())
+        else:
+            new_virtual_path = destination_path / source_path.name
+            self.would_exits.add(new_virtual_path.resolve())     
+
+    def validate_move_and_update_state(self, action_data : dict[Any, Any]):
         pass
+
 
 
     def validate_sequence_operation(self):
@@ -31,3 +99,10 @@ class SequenceOperations:
                 case _:
                     raise InvalidWorkFlowScript(f"Invalid operation for action {action_data.get("id")}")
 
+    def validate_workspace_scope(self, workspace : Path, path : Path, inside : bool = True):
+        """checks if the action path is outside the workspace. to prevent operations outside of workspace"""
+        if not FileSafety.is_relative_to(path1=workspace, path2=path):
+            raise OutOfScope("Path is outside the configured workspace. Choose a path inside the workspace or change --workspace.")
+        if inside:
+            if FileSafety.same_path(path1=workspace, path2=path):
+                raise WorkspaceProtection("This operation cannot target the workspace root itself. Choose an item inside the workspace.")
